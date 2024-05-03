8f87c182728e2e9d157a117e6c2895ee7987910d
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-03 11:42:13 +0200

    [SNACK2]

---
+++ snack2/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// SNACK 2
+
+/*
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+const word1 = prompt('Inserisci la prima parola');
+const word2 = prompt('Inserisci la seconda parola');
+
+if (word1.length < word2.length) {
+    console.log(word1, word2);
+} else if (word2.length < word1.length) {
+    console.log(word2, word1);
+} else {
+    console.log(word1, word2);
+}
